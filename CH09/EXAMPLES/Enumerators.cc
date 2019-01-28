622b8546223b8dc9a19e87bf9381d2c72538b551
lfresco <loryfresco@gmail.com>
2019-01-28 13:01:41 +0100

    added function that converts an integer into the right month

--- CH09/EXAMPLES/Enumerators.cc
+++ CH09/EXAMPLES/Enumerators.cc
@@ -15,8 +15,19 @@ int main (int argv, char * argc[]){
 	Month m2 = feb; 
 	m = 7;
 	int n = m;
-	Month mm = Month(7); 
+	Month mm = Month(7); // In this case the conversion will not work as the int type doesn't have a Month equivalent while Month has its integer equivalent.
+	
 
 
 	return 0;
 }
+
+Month int_to_Month(int x) {
+	if(x > int(Month::jan) || int(Month::dec) > x){
+		return Month(x);
+	} else {
+		std::cout << "Error!" << std::endl;
+	}
+} 
+
+
