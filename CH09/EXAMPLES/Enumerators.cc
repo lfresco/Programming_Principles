76d7f90c07c8de4cdf9782244ffe81e36afeb23f
lfresco <loryfresco@gmail.com>
2019-01-29 09:24:20 +0100

    Aggiunto Plain Enum

--- CH09/EXAMPLES/Enumerators.cc
+++ CH09/EXAMPLES/Enumerators.cc
@@ -8,6 +8,10 @@ enum class Day {
 	monday, tuesday, wednesday, thursday, friday, saturday, sunday
 };
 
+enum Month {
+	jan = 1, feb, mar, apr, may, jun, jul, aug, sep, opt, nov, dec
+};
+
 int main (int argv, char * argc[]){
 
 	Month m = Month::feb;
@@ -16,7 +20,11 @@ int main (int argv, char * argc[]){
 	m = 7;
 	int n = m;
 	Month mm = Month(7); // In this case the conversion will not work as the int type doesn't have a Month equivalent while Month has its integer equivalent.
-	
+	Month m = feb;  // Si può fare perchè feb è in scope
+	Month m2 = Month::feb; // anche questo è ok
+	m = 7;  // error : non si può assegnare un intero ad un mese
+	int n = m; // però si può convertire un mese in un intero
+	Month mm = Month(7); // OK: conversione implicita (non è controllata!!)	
 
 
 	return 0;
