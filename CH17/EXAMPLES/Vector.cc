79f5669048553cad06e35e06e9aa1eda81e02be5
lfresco <loryfresco@gmail.com>
2019-01-31 11:36:00 +0100

    Added constructor adn destructor

--- CH17/EXAMPLES/Vector.cc
+++ CH17/EXAMPLES/Vector.cc
@@ -7,8 +7,17 @@ class vector {
 		int sz;          //la dimensione del vettore
 		double* elem;    //pointer al primo elemento (in questo caso un double)
 	public:
-		vector(int s);  //constructor: allocherà memoria per s doubles e fa si che elem punti ad essi, e salverà s in sz
+		vector(int s)   //costruttore
+			:sz{s},  //inzializza la dimensione del vettore
+			elem{new double[s]} // inizializza elem come puntatore ad
+					    // un array di doubles di dimensione s
+			{
+				for (int i = 0; i < s; ++i) elem[i] = 0; // iniziali
+									//za gli ele
+									//menti
+			}
 		int size() const { return sz;} //restituisce la dimensione del nostro vettore
-
+		~vector() // destructor 
+			{ delete[] elem; } //libera la memoria
 };
 
