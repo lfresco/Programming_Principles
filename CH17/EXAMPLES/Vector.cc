28c99dc5377bf3ca201721563bf7e1e030c4f770
lfresco <loryfresco@gmail.com>
2019-01-31 14:58:47 +0100

    Added destructor

--- CH17/EXAMPLES/Vector.cc
+++ CH17/EXAMPLES/Vector.cc
@@ -19,5 +19,19 @@ class vector {
 		int size() const { return sz;} //restituisce la dimensione del nostro vettore
 		~vector() // destructor 
 			{ delete[] elem; } //libera la memoria
+
+		double get(int n) const {return elem[n]; } // access: read
+		void set(int n, double v) {elem[n] = v; } //access : write
 };
 
+int main (int argv, char * argc[]) {
+
+	vector v(5);
+
+	for (int i = 0; i < v.size(); ++i) {
+		v.set(i, 1.1 * i);
+		std::cout << "v[" << i <<"]==" << v.get(i) << std::endl;
+	}
+
+	return 0;
+}
