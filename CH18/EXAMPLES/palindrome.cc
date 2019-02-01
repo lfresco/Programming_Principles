53a048a60b169cdd1180318488a11de39b6b4d3a
lfresco <loryfresco@gmail.com>
2019-02-01 23:36:14 +0100

    Added is_palindrome that uses arrays

--- CH18/EXAMPLES/palindrome.cc
+++ CH18/EXAMPLES/palindrome.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 /*
 
 */
@@ -17,12 +18,39 @@ bool is_palindrome (const std::string& s)
 	return true;
 }
 
+bool is_palindrome(const char s[], int n) {
+	int first = 0;
+	int last = n - 1;
+	while(first < last) {
+		if(s[first] != s[last]) return false;
+		++first;
+		--last;
+	}
+
+	return true;
+}
+
+std::istream& read_word(std::istream& is, char * buffer, int max) {
+	is.width(max);
+	is >> buffer;
+
+	return is;
+}
+
 
 int main(int argv, char * argc[]) {
-
+	/*
 	for(std::string s; std::cin >> s;) {
 		std::cout << s << " is " ;
 		if(!is_palindrome(s)) std::cout << "not" ;
 		std::cout << " a palindrome\n" ;
+	}
+	*/
+
+	constexpr int max = 128;
+	for(char s[max]; read_word(std::cin, s, max); ){
+		std::cout << s << " is ";
+		if(!is_palindrome(s, std::strlen(s))) std::cout << "not ";
+		std::cout << "a palindrome\n";
 	}
 }
